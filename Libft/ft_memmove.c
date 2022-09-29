58c1d53363b6de8ec624e52aeeae94149e2d5456
al-arroyo <alarroyo@student.42madrid.com>
2022-09-29 20:45:02 +0200

    Itoa

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -38,13 +38,11 @@ static void	ft_loop_end(char *a, char *b, size_t n)
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t	i;
 	char	*a;
 	char	*b;
 
 	a = (char *)dest;
 	b = (char *)src;
-	i = 0;
 	if (!dest && !src)
 		return (NULL);
 	if (a < b)
