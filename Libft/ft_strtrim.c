ff831c09c21dd9d46c581c54a270a9f840c468d3
al-arroyo <alarroyo@student.42madrid.com>
2022-10-05 12:03:10 +0200

    ft_strtrim

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -6,79 +6,53 @@
 /*   By: alvaro <alvaro@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 12:49:25 by alarroyo          #+#    #+#             */
-/*   Updated: 2022/09/30 20:47:43 by alvaro           ###   ########.fr       */
+/*   Updated: 2022/10/05 11:58:23 by alvaro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_first_cut(char *s1, char const *set)
+static int	start(char *s1, char const *set)
 {
 	size_t	i;
-	int		j;
-	int		x;
-	char	*str;
+	size_t	len;
 
+	len = ft_strlen(s1);
 	i = 0;
-	j = 0;
-	str = s1;
-	while (s1[i] != 0 && i < ft_strlen(set))
-	{
-		x = 0;
-		while (set[x] != 0)
-		{
-			if (s1[i] != set[x])
-			{
-				str[j] = s1[i];
-				j++;
-			}
-			x++;
-		}
-		i++;
-	}
-	str[i] = 0;
-	return (str);
+	while (ft_strchr(set, s1[i]) != 0)
+		i ++;
+	return (i);
 }
 
-static char	*ft_last_cut(char *s1, char const *set)
+static int	end(char *s1, char const *set)
 {
-	int		i;
-	int		j;
-	int		x;
-	int		z;
-	char	*str;
+	size_t	len;
+	size_t	j;
 
-	str = s1;
-	i = ft_strlen(s1) - 1;
-	j = ft_strlen(s1) - 1;
-	z = ft_strlen(set);
-	while (i-- >= 0 && z-- >= 0)
-	{
-		x = 0;
-		while (set[x] != 0)
-		{
-			if (s1[i] != set[x])
-			{
-				str[j] = s1[i];
-				j--;
-			}
-			x++;
-		}
-	}
-	return (str);
+	len = ft_strlen(s1);
+	j = 0;
+	while (ft_strchr(set, s1[len - j - 1]) != 0)
+		j++;
+	return (len - j);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	int		i;
+	int		j;
 	char	*aux;
 
-	if (!s1 || !set)
+	if (!s1)
 		return (NULL);
-	aux = ft_calloc(sizeof(char *), ft_strlen(s1));
+	if (!set)
+		return (ft_strdup(s1));
+	i = start(s1, set);
+	j = end(s1, set);
+	if (i >= j)
+		return (ft_strdup(""));
+	aux = ft_calloc(sizeof(char *), j - i + 1);
 	if (!aux)
 		return (NULL);
-	aux = (char *) s1;
-	ft_first_cut(aux, set);
-	ft_last_cut(aux, set);
+	ft_strlcpy(aux, s1 + i, j - i + 1);
 	return (aux);
 }
--- README.md
+++
@@ -1,6 +0,0 @@
-# Libft
-# Libft
-# Libft
-# Libft
-# Libft
-# Libft
